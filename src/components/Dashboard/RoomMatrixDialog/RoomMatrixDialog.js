b93a2257d954b3eb7928008b00d5f4403ba3aaad
Sudarshan <2gi18me135@students.git.edu>
2024-08-09 12:26:03 +0530

    ttt

--- src/components/Dashboard/RoomMatrixDialog/RoomMatrixDialog.js
+++ src/components/Dashboard/RoomMatrixDialog/RoomMatrixDialog.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './roomMatrixDialog.css'; // Import the CSS file
+import React from "react";
+import "./roomMatrixDialog.css"; // Import the CSS file
 
 const RoomMatrixDialog = ({ open, onClose, pgDetails, payingGuests }) => {
   // Initialize room status with total beds based on pgDetails
   const roomStatus = {};
-  
+
   let roomCounter = 1; // Initialize room number counter
 
   // Set up initial status for each floor and room
@@ -14,19 +14,23 @@ const RoomMatrixDialog = ({ open, onClose, pgDetails, payingGuests }) => {
       roomStatus[floor][roomCounter] = {
         single: pgDetails.singleBedsPerRoom,
         double: pgDetails.doubleSharingBedsPerRoom,
-        triple: pgDetails.tripleSharingBedsPerRoom
+        triple: pgDetails.tripleSharingBedsPerRoom,
       };
       roomCounter++; // Increment room number for each room
     }
   }
 
   // Update roomStatus based on payingGuests
-  payingGuests.forEach(guest => {
+  payingGuests.forEach((guest) => {
     const { floorNo, roomNo, roomType } = guest;
-    const roomTypeMapping = { 'S': 'single', 'D': 'double', 'T': 'triple' };
+    const roomTypeMapping = { S: "single", D: "double", T: "triple" };
     const type = roomTypeMapping[roomType];
 
-    if (roomStatus[floorNo] && roomStatus[floorNo][roomNo] && roomStatus[floorNo][roomNo][type] !== undefined) {
+    if (
+      roomStatus[floorNo] &&
+      roomStatus[floorNo][roomNo] &&
+      roomStatus[floorNo][roomNo][type] !== undefined
+    ) {
       roomStatus[floorNo][roomNo][type] -= 1;
     }
   });
@@ -34,44 +38,57 @@ const RoomMatrixDialog = ({ open, onClose, pgDetails, payingGuests }) => {
   // Function to calculate total available beds
   const calculateTotalAvailableBeds = (type) => {
     return Object.values(roomStatus).reduce((total, floor) => {
-      return total + Object.values(floor).reduce((floorTotal, room) => {
-        return floorTotal + (room[type] > 0 ? room[type] : 0);
-      }, 0);
+      return (
+        total +
+        Object.values(floor).reduce((floorTotal, room) => {
+          return floorTotal + (room[type] > 0 ? room[type] : 0);
+        }, 0)
+      );
     }, 0);
   };
 
-  const totalAvailableSingle = calculateTotalAvailableBeds('single');
-  const totalAvailableDouble = calculateTotalAvailableBeds('double');
-  const totalAvailableTriple = calculateTotalAvailableBeds('triple');
+  const totalAvailableSingle = calculateTotalAvailableBeds("single");
+  const totalAvailableDouble = calculateTotalAvailableBeds("double");
+  const totalAvailableTriple = calculateTotalAvailableBeds("triple");
 
   // Check if there are any rooms with data
-  const hasData = Object.keys(roomStatus).some(floor =>
-    Object.keys(roomStatus[floor]).some(room => {
+  const hasData = Object.keys(roomStatus).some((floor) =>
+    Object.keys(roomStatus[floor]).some((room) => {
       const { single, double, triple } = roomStatus[floor][room];
-      return (single >= 0 || double >= 0 || triple >= 0);
+      return single >= 0 || double >= 0 || triple >= 0;
     })
   );
 
   if (!hasData) {
     return (
-      <div className="room-matrix-dialog-overlay" style={{ display: open ? 'flex' : 'none' }}>
+      <div
+        className="room-matrix-dialog-overlay"
+        style={{ display: open ? "flex" : "none" }}
+      >
         <div className="room-matrix-dialog">
           <div className="dialog-header">
             <div className="dialog-title">Room Matrix</div>
-            <button className="close-button" onClick={onClose}>×</button>
+            <button className="close-button" onClick={onClose}>
+              ×
+            </button>
           </div>
-          <div className='empty'>No data available</div>
+          <div className="empty">No data available</div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="room-matrix-dialog-overlay" style={{ display: open ? 'flex' : 'none' }}>
+    <div
+      className="room-matrix-dialog-overlay"
+      style={{ display: open ? "flex" : "none" }}
+    >
       <div className="room-matrix-dialog">
         <div className="dialog-header">
           <div className="dialog-title">Room Matrix</div>
-          <button className="close-button1" onClick={onClose}>×</button>
+          <button className="close-button1" onClick={onClose}>
+            ×
+          </button>
         </div>
         <div className="legend">
           <div className="legend-item">
@@ -85,34 +102,60 @@ const RoomMatrixDialog = ({ open, onClose, pgDetails, payingGuests }) => {
         </div>
         <div className="dialog-content">
           <div className="totals">
-            <div>Total Available Beds</div>
-            <div>Single Beds: {totalAvailableSingle}</div>
-            <div>Double Beds: {totalAvailableDouble}</div>
-            <div>Triple Beds: {totalAvailableTriple}</div>
+            <div className="total-beds-heading">Total Available Beds</div>
+            <div>
+              Single Beds: <span className="value">{totalAvailableSingle}</span>
+            </div>
+            <div>
+              Double Beds: <span className="value">{totalAvailableDouble}</span>
+            </div>
+            <div>
+              Triple Beds: <span className="value">{totalAvailableTriple}</span>
+            </div>
           </div>
+
           <div className="room-matrix">
             {[...Array(pgDetails.totalFloors)].map((_, floorIndex) => {
               const floor = floorIndex + 1;
               return (
                 <div key={floor} className="floor1">
-                  {[...Object.keys(roomStatus[floor])].map(roomNo => {
-                    const room = (roomStatus[floor] && roomStatus[floor][roomNo]) || { single: -1, double: -1, triple: -1 };
+                  {[...Object.keys(roomStatus[floor])].map((roomNo) => {
+                    const room = (roomStatus[floor] &&
+                      roomStatus[floor][roomNo]) || {
+                      single: -1,
+                      double: -1,
+                      triple: -1,
+                    };
                     return (
                       <div key={roomNo} className="room-container">
-                        <div className="room-header">F{floor}R{roomNo}</div>
+                        <div className="room-header">
+                          F{floor}R{roomNo}
+                        </div>
                         <div className="room-content">
                           {room.single !== -1 && (
-                            <div className={`room-compartment ${room.single === 0 ? 'filled' : 'available'}`}>
+                            <div
+                              className={`room-compartment ${
+                                room.single === 0 ? "filled" : "available"
+                              }`}
+                            >
                               {room.single}(S)
                             </div>
                           )}
                           {room.double !== -1 && (
-                            <div className={`room-compartment ${room.double === 0 ? 'filled' : 'available'}`}>
+                            <div
+                              className={`room-compartment ${
+                                room.double === 0 ? "filled" : "available"
+                              }`}
+                            >
                               {room.double}(D)
                             </div>
                           )}
                           {room.triple !== -1 && (
-                            <div className={`room-compartment ${room.triple === 0 ? 'filled' : 'available'}`}>
+                            <div
+                              className={`room-compartment ${
+                                room.triple === 0 ? "filled" : "available"
+                              }`}
+                            >
                               {room.triple}(T)
                             </div>
                           )}
